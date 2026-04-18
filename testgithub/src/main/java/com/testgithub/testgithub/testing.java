5175ba827abaf7be044a653f52681ea527a9b6c4
kumar <kumar001@gmail.com>
2026-04-18 09:53:47 +0200

    first commit

--- testgithub/src/main/java/com/testgithub/testgithub/testing.java
+++ testgithub/src/main/java/com/testgithub/testgithub/testing.java
@@ -2,6 +2,6 @@ package com.testgithub.testgithub;
 
 public class testing {
     public static void main(String[] args) {
-        System.out.println("hello world from murugan 001");
+        System.out.println("hello world for murugan");
     }
 }
