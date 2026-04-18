687b8d9bda77f9999ae0c0b6b2734fb87d55148a
kumar <kumar001@gmail.com>
2026-04-18 10:16:00 +0200

    test cherry pick

--- testgithub/src/main/java/com/testgithub/testgithub/testing.java
+++ testgithub/src/main/java/com/testgithub/testgithub/testing.java
@@ -3,5 +3,6 @@ package com.testgithub.testgithub;
 public class testing {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("test cherry pick");
     }
 }
